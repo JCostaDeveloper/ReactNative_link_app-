4b07dfd73a9b280de50b1d0b24dbf9c56bc1d9e2
joao <costadesenv@gmail.com>
2025-11-01 21:27:18 -0300

    crteate header of app

--- app/src/app/index/index.tsx
+++ app/src/app/index/index.tsx
@@ -1,8 +1,27 @@
-import {Text}from 'react-native';//import de componentes do react-native
+import { View, Image, TouchableOpacity } from 'react-native';//import de componentes do react-native.
+
+import {MaterialIcons} from "@expo/vector-icons";//import de icones do expo vector icons.
+
+import { colors } from '@/styles/colors';//importa as cores do app.
 
 import { styles } from './styles';
 
-//Rota inicial (Index) do app, por ser export default pode encontrada pelo expo router 
+//Rota inicial (Index) do app, por ser export default pode ser encontrada pelo expo router. 
 export default function Index(){ 
-    return <Text style = { styles.title } > Hello World! React Native</Text>
+
+    return(
+    
+    <View style ={styles.conteiner}>
+        <View style = {styles.header}>
+            <Image
+                source={require('@/assets/logo.png')}
+            style ={styles.logo}/>
+            <TouchableOpacity>
+                <MaterialIcons name="add" size= {32} color={ colors.green[300]}/>
+            </TouchableOpacity>
+        </View>
+    </View>
+
+    )
+
 }
--- app/src/app/index/styles.tsx
+++ app/src/app/index/styles.tsx
@@ -3,7 +3,22 @@ import { StyleSheet } from "react-native"
 import { colors } from "@/styles/colors";
     
 export const styles = StyleSheet.create({
-    title:{
-        color: colors.green[900]
+    conteiner:{
+        flex:1,
+        paddingTop: 42
+
+    },
+    header:{
+        paddingHorizontal:24, 
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 32
+    },
+    logo:{
+        height:32,
+        width: 38
+        
     }
 });
